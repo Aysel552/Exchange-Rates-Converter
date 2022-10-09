9c910c658b4bb7036764c3ec726ab89d2269b16f
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-10-09 22:08:14 +0400

    adding a structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const MY_KEY = "UUfzccY0vDiZmqRGAZCv5bxaCGQiXkFI";
+
+  useEffect(() =>{
+    const getRate = async() => {
+      const response = await fetch(`https://api.apilayer.com/currency_data/live?base=USD&symbols=EUR,GBP`)
+    }
+  })
   return (
     <div className="App">
-      
+     <h2>Adding</h2>
     </div>
   );
 }
